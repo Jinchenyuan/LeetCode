ecdb5b7e90891723e00a8c0f089c31fb32e73fe4
jinchenyuan <jinchenyuan@outlook.com>
2019-09-26 02:35:57 +0800

    problem 89 引用左耳朵耗子的解法.

--- src/89.gray-code.cpp
+++ src/89.gray-code.cpp
@@ -9,7 +9,16 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> grayCode(int n) {
+    //左耳朵耗子的解法，参见：https://github.com/haoel/leetcode/blob/master/algorithms/cpp/grayCode/grayCode.cpp
+    vector<int> grayCode1(int n) {
+        vector<int> ret;   
+        int size = 1 << n;   
+        for(int i = 0; i < size; ++i) {
+            ret.push_back((i >> 1)^i);   
+        }
+        return ret;
+    }
+    vector<int> grayCode2(int n) {
         vector<int> ans;
         ans.push_back(0);
         if(n == 0) 
