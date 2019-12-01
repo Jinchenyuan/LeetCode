940f7e3292392821b20027e1f6e521bc3c87b0c9
jinchenyuan <jinchenyuan@outlook.com>
2019-12-01 20:35:16 +0800

    commit problem 224.

--- src/224.basic-calculator.cpp
+++ src/224.basic-calculator.cpp
@@ -8,7 +8,6 @@
 /*
  * 使用栈来解答，从左到右，遇到优先级跟高的入栈
  * 结束高优先级则出栈，并且把结果再次与栈尾结算。
- * 遍历完所有符号，则栈中仅剩一个值，即为最终结果。
  * 
  * */
 
@@ -16,18 +15,51 @@
 #include <stack>
 using namespace std;
 
-struct Node
-{
-    long long val;
-    int op;
-};
-
-
 class Solution {
 public:
     int calculate(string s) {
-        long long ret = 0;
-        stack<Node> st;
+        int ret = 0;
+        stack<int> st;
+        size_t sLen = s.size();
+        int op = 1;
+        for (size_t i = 0; i < sLen; ++i)
+        {
+            char c = s[i];
+            if (c == '-')
+            {
+                op = -1;
+            }
+            else if (c == '+')
+            {
+                op = 1;
+            }
+            else if (s[i] == '(')
+            {
+                st.push(ret);
+                st.push(op);
+                ret = 0;
+                op = 1;
+            }
+            else if (s[i] == ')')
+            {
+                ret *= st.top();
+                st.pop();
+                ret += st.top();
+                st.pop();
+            }
+            else if (c >= '0' && c <= '9')
+            {
+                int num = 0;
+                while (s[i] >= '0' && s[i] <= '9' && i < sLen)
+                {
+                    num = num * 10 + (s[i++] - '0');
+                }
+                ret += op * num;
+                i--;
+            }
+        }
+        
+        return ret;
     }
 };
 // @lc code=end
