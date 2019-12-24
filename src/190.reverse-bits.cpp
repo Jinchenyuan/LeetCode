07f3a010d1b6e8c20de958fe56efca6d3e782508
chenyuan.jin <chenyuan.jin@capstones.cn>
2019-12-24 16:43:12 +0800

    improve problem 190.

--- src/190.reverse-bits.cpp
+++ src/190.reverse-bits.cpp
@@ -31,7 +31,7 @@ public:
         for (int i = 0; i < 32; ++i)
         {
             ret = ret << 1;
-            ret += n & 1;
+            ret |= n & 1;
             n = n >> 1;
         }
         return ret;
