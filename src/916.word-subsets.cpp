09b77fcb469f1b815aae67d69df7d43faaca69ef
chenyuan.jin <chenyuan.jin@capstones.cn>
2020-12-31 20:21:16 +0800

    commit problem 916 uncomplete.

---
+++ src/916.word-subsets.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=916 lang=cpp
+ *
+ * [916] Word Subsets
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
+        
+    }
+
+    string wordCotainChar(string str, char c) {
+        int len = str.length();
+        for (int i = 0; i < len; i++)
+        {
+            if ()
+        }
+        
+    }
+};
+// @lc code=end
+
